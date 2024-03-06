daccff3c12f2f6b2f9ea455d980eea317fb3c6a4
Meerkat1030 <hong991000@gmail.com>
2024-03-06 12:35:58 +0900

    [COMMIT] 03/06 commit React 쇼핑몰 예제 연습

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+/*eslint-disable*/
+import React, { useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Card, Container, Nav, Navbar } from 'react-bootstrap';
+import bg from './bg_main.jpg';
+import data from './data.js';
+
+console.log(data);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [items, setItems] = useState(data);
 
+    return (
+        <div className="App">
+            <Navbar bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+            <div className="main-bg"></div>
+
+            {/*컴포넌트화 한후 데이터 바인딩 시킨 버전*/}
+            <ItemsList items={items}></ItemsList>
+
+            {/*컴포넌트화 안시킨 하드코딩 버전*/}
+            {/*<div className="container">*/}
+            {/*    <div className="row">*/}
+            {/*        <div className="col-md-4">*/}
+            {/*            <div className="image-box">*/}
+            {/*                <img className="image-thumbnail" src={process.env.PUBLIC_URL+'/item1.jpg'}></img>*/}
+
+            {/*            </div>*/}
+            {/*            <h4>{items[0].title}</h4>*/}
+            {/*            <p>{items[0].content}</p>*/}
+            {/*        </div>*/}
+            {/*        <div className="col-md-4">*/}
+            {/*            <div className="image-box">*/}
+            {/*                <img className="image-thumbnail" src={process.env.PUBLIC_URL + '/item2.jpg'}></img>*/}
+
+            {/*            </div>*/}
+            {/*            <h4>{items[1].title}</h4>*/}
+            {/*            <p>{items[1].content}</p>*/}
+            {/*        </div>*/}
+            {/*        <div className="col-md-4">*/}
+            {/*            <div className="image-box">*/}
+            {/*                <img className="image-thumbnail" src={process.env.PUBLIC_URL + '/item3.jpg'}></img>*/}
+
+            {/*            </div>*/}
+            {/*            <h4>{items[2].title}</h4>*/}
+            {/*            <p>{items[2].content}</p>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+        </div>
+    );
+}
+function ItemsList({ items }) {
+    return (
+        <div className="container">
+            <div className="row">
+                {items.map((item, index) => (
+                    <div className="col-md-4" key={index}>
+                        <div className="image-box">
+                            <img className="image-thumbnail" src={`${process.env.PUBLIC_URL}/${`item${index + 1}.jpg`}`} alt={`item-${index + 1}`}></img>
+                        </div>
+                        <h4>{item.title}</h4>
+                        <p>{item.content}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
 export default App;
---
+++ src/data.js
@@ -0,0 +1,24 @@
+let data = [
+    {
+        id : 0,
+        title : "송우기1",
+        content : "손가락 깨물 송우기",
+        price : 120000
+    },
+
+    {
+        id : 1,
+        title : "송우기2",
+        content : "핑크빛 탈색머리 송우기",
+        price : 110000
+    },
+
+    {
+        id : 2,
+        title : "송우기3",
+        content : "공항패션 송우기",
+        price : 130000
+    }
+]
+
+export default data
