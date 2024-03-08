8030bc82dac919b5f7e389eb23313b45ec1aec0b
Meerkat1030 <hong991000@gmail.com>
2024-03-08 12:08:23 +0900

    [COMMIT] 03/08 commit React styled-components 연습

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,6 +1,13 @@
 import React from "react";
 import {useParams} from "react-router-dom";
+import styled from "styled-components";
 
+let YellowBtn = styled.button`
+// css 작성
+    background: ${props => props.bg};
+    color : ${props => props.bg == 'blue' ? 'white' : 'black'};
+    padding: 10px;
+`
 function Detail(props){
     let {id} = useParams();
     let findItem = props.items.find(function (x){
@@ -19,6 +26,9 @@ function Detail(props){
                     <p>{props.items[id].price}</p>
                     <button className="btn btn-danger">주문하기</button>
                 </div>
+
+                <YellowBtn bg = "yellow">버튼</YellowBtn>
+                <YellowBtn bg = "blue">버튼</YellowBtn>
             </div>
         </div>
     )
